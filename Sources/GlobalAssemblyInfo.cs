ec256d5f6e938922c588bef542ebf30cde208b20
Julien Blin <julien.blin@gmail.com>
2010-01-11 14:31:22 -0500

    Initial commit with empty entity project

---
+++ Sources/GlobalAssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Russell")]
+[assembly: AssemblyProduct("Russell.RADAR.POC")]
+[assembly: AssemblyCopyright("Copyright © Russell 2010")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
