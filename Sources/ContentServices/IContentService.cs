14bac7ab462bb6db6795c5ed964fc28cb5b719f2
unknown <bliju1@.qc.cgi.ca>
2010-01-18 11:31:31 -0500

    Added ContentServices project

---
+++ Sources/ContentServices/IContentService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DocumentFormat.OpenXml.Wordprocessing;
+
+namespace Russell.RADAR.POC.ContentServices
+{
+    public interface IContentService
+    {
+        string StripXHTML(string input);
+        Paragraph CreateWordParagraphFromXHTML(string input);
+    }
+}
